e819357019468efd93423d94a340d6878f8d83c6
Alireza Rahnama <74360032+Alirezarahnamaa@users.noreply.github.com>
2023-09-21 00:49:07 +0330

    Add files via upload

---
+++ Frame_check.py
@@ -0,0 +1,29 @@
+def Algoritm(frameOne, frameTwo):
+    cMul = 2.0*frameOne * frameTwo
+    cPow = pow(frameOne, 2.0)+pow(frameTwo, 2.0)
+    cDivid = np.true_divide(cMul, cPow, where=(cMul != 0) | (cPow != 0))
+    cMean = np.mean(cDivid)
+    
+    return float("{:.4f}".format(cMean))
+
+def FrameCheck(Video):
+    finalFrame = []
+    frameLenght = len(Video)
+
+    finalFrame.append(np.asarray(Video[0]))
+    tempFrame = finalFrame[0]
+    tempResult = []
+    tempResult.append(Algoritm(tempFrame, Video[1]))
+
+    for frameIndex in range(2, frameLenght-1):
+        algoritmResult = Algoritm(tempFrame, Video[frameIndex])
+        if np.mean(tempResult) > algoritmResult:
+        #if np.mean(tempResult[-10:]) > algoritmResult:
+            finalFrame.append(np.asarray(Video[frameIndex]))
+            tempFrame = Video[frameIndex]
+            tempResult.append(algoritmResult)
+
+    print('mean: '+str(np.mean(tempResult)))
+    print(str(frameLenght-len(finalFrame))+' frams is removed')
+
+    return finalFrame
