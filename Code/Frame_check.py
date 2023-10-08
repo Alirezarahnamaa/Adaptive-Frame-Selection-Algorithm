ea0cfa6cef563dd465af798da4a9f8cf995309d6
Alireza Rahnama <74360032+Alirezarahnamaa@users.noreply.github.com>
2023-10-08 18:20:05 +0330

    Update Frame_check.py

--- Code/Frame_check.py
+++ Code/Frame_check.py
@@ -1,6 +1,6 @@
 def Algoritm(frameOne, frameTwo):
     cMul = 2.0*frameOne * frameTwo
-    cPow = pow(frameOne, 2.0)+pow(frameTwo, 2.0)
+    cPow = np.sqrt(pow(frameOne, 2.0)+pow(frameTwo, 2.0))
     cDivid = np.true_divide(cMul, cPow, where=(cMul != 0) | (cPow != 0))
     cMean = np.mean(cDivid)
     
